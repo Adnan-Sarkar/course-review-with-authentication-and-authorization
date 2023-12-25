ec506954bb11f8b750fe7f3fa518cbfcd5ada26f
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-12-26 02:53:19 +0600

    Added env file info into config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,5 +7,8 @@ dotenv.config({
 
 export default {
   PORT: process.env.PORT,
+  SALT_ROUND: process.env.SALT_ROUND,
   DB_URL: process.env.DB_URL,
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+  JWT_ACCESS_TOKEN_EXPIRES_IN: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
 };
