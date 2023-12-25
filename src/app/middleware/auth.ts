1083d3bb16f2f11f37fd203fb4d43230d9ba1cf8
Adnan-Sarkar <adnansarkar111@gmail.com>
2023-12-25 20:53:15 +0600

    Created auth middleware to ensure authorized user can use the resource

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,36 @@
+import httpStatus from "http-status";
+import AppError from "../error/AppError";
+import { TUserRoles } from "../modules/user/user.interface";
+import catchAsync from "../utils/catchAsync";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+
+const auth = (...permittedRoles: TUserRoles[]) => {
+  return catchAsync(async (req, _res, next) => {
+    // check if the token availabe
+    const token = req.headers.authorization;
+
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!!!");
+    }
+
+    // check if the token is valid
+    const decode = jwt.verify(
+      token,
+      config.JWT_ACCESS_TOKEN_SECRET as string,
+    ) as JwtPayload;
+
+    // check if the role is authorized
+    const { role } = decode;
+
+    if (permittedRoles && !permittedRoles.includes(role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!!!");
+    }
+
+    req.user = decode;
+
+    return next();
+  });
+};
+
+export default auth;
